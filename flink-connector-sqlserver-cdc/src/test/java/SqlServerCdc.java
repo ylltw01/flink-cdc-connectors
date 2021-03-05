fa5a7420a0ff525e47e9d6bd61e2742b0199d90d
noone <ylltw01@163.com>
2021-03-05 10:02:30 +0800

    sqlserver cdc

--- flink-connector-sqlserver-cdc/src/test/java/SqlServerCdc.java
+++ flink-connector-sqlserver-cdc/src/test/java/SqlServerCdc.java
@@ -139,3 +139,148 @@ public class SqlServerCdc {
     }
 
 }
+
+/*
+-- SQLServer 建表
+CREATE TABLE test.dbo.sqlserver_cdc (
+	id int NOT NULL,
+	name varchar(100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	description varchar(100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	weight varchar(100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	create_time datetime NULL,
+	CONSTRAINT sqlserver_cdc_PK PRIMARY KEY (id)
+) GO
+
+
+CREATE TABLE test.dbo.sqlserver_all_type_cdc (
+	id int NOT NULL,
+	bigint_2 bigint NULL,
+	bit_3 bit NULL,
+	char_4 char(100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	date_5 date NULL,
+	datetime_6 datetime NULL,
+	datetime2_7 datetime2(7) NULL,
+	decimai_8 decimal(18,0) NULL,
+	float_9 float NULL,
+	money_10 money NULL,
+	nchar_11 nchar(100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	ntext_12 ntext(1073741823) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	numeric_13 numeric(18,0) NULL,
+	nvarchar_14 nvarchar(100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	real_15 real NULL,
+	samlldatatime_16 smalldatetime NULL,
+	text_17 text(2147483647) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	time_18 time NULL,
+	tinyint_20 tinyint NULL,
+	varchar_21 varchar(100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	xml_22 xml NULL,
+	timestamp_19 timestamp(8) NULL,
+	CONSTRAINT sqlserver_all_type_cdc_PK PRIMARY KEY (id)
+) GO
+
+
+-- MySQL 建表
+CREATE TABLE `mysql_sqlserver_cdc` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) COLLATE utf8_bin DEFAULT NULL,
+  `description` varchar(100) COLLATE utf8_bin DEFAULT NULL,
+  `weight` varchar(100) COLLATE utf8_bin DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
+
+
+CREATE TABLE `mysql_sqlserver_all_type_cdc` (
+  `id` int(11) NOT NULL,
+  `bigint_2` bigint(20) DEFAULT NULL,
+  `bit_3` int(11) DEFAULT NULL,
+  `char_4` char(100) COLLATE utf8_bin DEFAULT NULL,
+  `date_5` date DEFAULT NULL,
+  `datetime_6` datetime DEFAULT NULL,
+  `datetime2_7` datetime DEFAULT NULL,
+  `decimai_8` decimal(18,0) DEFAULT NULL,
+  `float_9` float DEFAULT NULL,
+  `money_10` decimal(18,0) DEFAULT NULL,
+  `nchar_11` varchar(100) COLLATE utf8_bin DEFAULT NULL,
+  `ntext_12` text COLLATE utf8_bin,
+  `numeric_13` decimal(18,0) DEFAULT NULL,
+  `nvarchar_14` varchar(100) COLLATE utf8_bin DEFAULT NULL,
+  `real_15` varchar(100) COLLATE utf8_bin DEFAULT NULL,
+  `samlldatatime_16` datetime DEFAULT NULL,
+  `text_17` text COLLATE utf8_bin,
+  `time_18` bigint(20) DEFAULT NULL,
+  `tinyint_20` int(11) DEFAULT NULL,
+  `varchar_21` varchar(100) COLLATE utf8_bin DEFAULT NULL,
+  `xml_22` varchar(100) COLLATE utf8_bin DEFAULT NULL,
+  `timestamp_19` varchar(100) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
+
+
+-- flink 语句，SQLServer timestamp 要映射为 String 类型
+ tableEnv.executeSql("CREATE TABLE sqlserver_all_type_cdc (" +
+                        " id INT NOT NULL," +
+                        " bigint_2 INT," +
+                        " bit_3 BOOLEAN," +
+                        " char_4 STRING," +
+                        " date_5 DATE," +
+                        " datetime_6 TIMESTAMP," +
+                        " datetime2_7 TIMESTAMP," +
+                        " decimai_8 DOUBLE," +
+                        " float_9 DOUBLE," +
+                        " money_10 DOUBLE," +
+                        " nchar_11 STRING," +
+                        " ntext_12 STRING," +
+                        " numeric_13 DOUBLE," +
+                        " nvarchar_14 STRING," +
+                        " real_15 STRING," +
+                        " samlldatatime_16 TIMESTAMP," +
+                        " text_17 STRING," +
+                        " time_18 INT NULL," +
+                        " tinyint_20 int NULL," +
+                        " varchar_21 STRING," +
+                        " xml_22 STRING," +
+                        " timestamp_19 STRING " +
+                ") WITH (" +
+                        " 'connector' = 'sqlserver-cdc'," +
+                        " 'hostname' = '10.0.8.69'," +
+                        " 'port' = '1433'," +
+                        " 'username' = 'sa'," +
+                        " 'password' = 'Cdsf@119'," +
+                        " 'database-name' = 'test'," +
+                        " 'table-name' = 'dbo.sqlserver_all_type_cdc'" +
+                        ")");
+
+        TableResult sinkMysql = tableEnv.executeSql("CREATE TABLE mysql_sqlserver_all_type_cdc (" +
+                " id INT NOT NULL," +
+                " bigint_2 INT," +
+                " bit_3 BOOLEAN," +
+                " char_4 STRING," +
+                " date_5 DATE," +
+                " datetime_6 TIMESTAMP," +
+                " datetime2_7 TIMESTAMP," +
+                " decimai_8 DOUBLE," +
+                " float_9 DOUBLE," +
+                " money_10 DOUBLE," +
+                " nchar_11 STRING," +
+                " ntext_12 STRING," +
+                " numeric_13 DOUBLE," +
+                " nvarchar_14 STRING," +
+                " real_15 STRING," +
+                " samlldatatime_16 TIMESTAMP," +
+                " text_17 STRING," +
+                " time_18 INT NULL," +
+                " tinyint_20 int NULL," +
+                " varchar_21 STRING," +
+                " xml_22 STRING," +
+                " timestamp_19 STRING, " +
+                " PRIMARY KEY (`id`) NOT ENFORCED " +
+                ") WITH (" +
+                " 'connector' = 'jdbc'," +
+                " 'url' = 'jdbc:mysql://10.0.8.45:3306/test_etl'," +
+                " 'username' = 'root', " +
+                " 'password' = 'Cdsf@119', " +
+                " 'table-name' = 'mysql_sqlserver_all_type_cdc' " +
+                ")");
+
+ */
